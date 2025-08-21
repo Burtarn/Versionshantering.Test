14deb249588acc172dc4c73b8302a1765331493c
Robin <robin.andersson0125@gmail.com>
2025-08-21 10:14:29 +0200

    Update App.tsx

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Robin</h1>
+      <h1>Vite + React + Robin + Dess</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           räknaren är {count}
